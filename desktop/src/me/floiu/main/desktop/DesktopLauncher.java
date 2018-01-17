0eaa7e1b7d783e93e203a330e5c48e473c6453b7
DESKTOP-LUKASZ\Lukasz <ratajaczak@gmail.com>
2018-01-17 17:44:09 +0100

    Launcher configuration

--- desktop/src/me/floiu/main/desktop/DesktopLauncher.java
+++ desktop/src/me/floiu/main/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package me.floiu.main.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import me.floiu.main.main;
+import me.floiu.main.Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new main(), config);
+
+		config.title = Main.GAME_NAME;
+		config.width = Main.WIDTH;
+		config.height = Main.HEIGHT;
+		config.resizable = false;
+
+		new LwjglApplication(new Main(), config);
 	}
 }
