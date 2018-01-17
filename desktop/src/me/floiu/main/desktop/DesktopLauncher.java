a385b518ddc168a799b846a61ca6dd6e005a8cef
DESKTOP-LUKASZ\Lukasz <ratajaczak@gmail.com>
2018-01-17 16:50:06 +0100

    init

---
+++ desktop/src/me/floiu/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.floiu.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.floiu.main.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+	}
+}
