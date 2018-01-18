e4fcb01042eb7659dda9adc601012f7e49212ea3
DESKTOP-LUKASZ\Lukasz <ratajaczak@gmail.com>
2018-01-18 18:40:44 +0100

    repair main file

--- core/src/me/floiu/main/main.java
+++ core/src/me/floiu/main/main.java
@@ -6,19 +6,19 @@ import me.floiu.main.screens.SplashScreen;
 
 public class Main extends Game {
 
-	public final static String GAME_NAME = "TicTacToe";
+    public final static String GAME_NAME = "TicTacToe";
 
-	public final static int WIDTH = 700;
-	public final static int HEIGHT = 700;
+    public final static int WIDTH = 700;
+    public final static int HEIGHT = 700;
 
     private boolean paused;
 
-	@Override
-	public void create () {
-		this.setScreen(new SplashScreen(this));
-	}
+    @Override
+    public void create () {
+        this.setScreen(new SplashScreen(this));
+    }
 
-	/**
+    /**
      * ---------------------
      *  GETTERS AND SETTERS
      * ---------------------
