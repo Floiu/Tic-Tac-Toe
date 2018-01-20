94eb4a3cf7980b2b6fee4301c4c02cbd999ff7d8
DESKTOP-LUKASZ\Lukasz <ratajaczak@gmail.com>
2018-01-20 18:30:41 +0100

    refactor find win system

--- core/src/me/floiu/main/GameController.java
+++ core/src/me/floiu/main/GameController.java
@@ -8,7 +8,9 @@ public class GameController {
     private char[] boardStatus = {'#', '#', '#', '#', '#', '#', '#', '#', '#'};
 
     private char whoIsNow = 'x';
+
     private float winBoardResetDelay = 0.75f;
+    private char whereWin;
     private boolean someoneWin = false;
 
     public void makeMove(int i, InputEvent event) {
@@ -22,14 +24,71 @@ public class GameController {
     }
 
     private void checkWin() {
-        if (((boardStatus[0] == whoIsNow) && (boardStatus[1] == whoIsNow)&&(boardStatus[2] == whoIsNow)) ||
-                ((boardStatus[3] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[5] == whoIsNow)) ||
-                ((boardStatus[6] == whoIsNow) && (boardStatus[7] == whoIsNow)&&(boardStatus[8] == whoIsNow)) ||
-                ((boardStatus[0] == whoIsNow) && (boardStatus[3] == whoIsNow)&&(boardStatus[6] == whoIsNow)) ||
-                ((boardStatus[1] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[7] == whoIsNow)) ||
-                ((boardStatus[2] == whoIsNow) && (boardStatus[5] == whoIsNow)&&(boardStatus[8] == whoIsNow)) ||
-                ((boardStatus[0] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[8] == whoIsNow)) ||
-                ((boardStatus[2] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[6] == whoIsNow))) {
+        if ((boardStatus[0] == whoIsNow) && (boardStatus[1] == whoIsNow) && (boardStatus[2] == whoIsNow)) {
+            whereWin = '_';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[3] == whoIsNow) && (boardStatus[4] == whoIsNow) && (boardStatus[5] == whoIsNow)) {
+            whereWin = '-';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[6] == whoIsNow) && (boardStatus[7] == whoIsNow) && (boardStatus[8] == whoIsNow)) {
+            whereWin = '⁻';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[0] == whoIsNow) && (boardStatus[3] == whoIsNow) && (boardStatus[6] == whoIsNow)) {
+            whereWin = '|';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[1] == whoIsNow) && (boardStatus[4] == whoIsNow) && (boardStatus[7] == whoIsNow)) {
+            whereWin = '!';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[2] == whoIsNow) && (boardStatus[5] == whoIsNow) && (boardStatus[8] == whoIsNow)) {
+            whereWin = ':';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[0] == whoIsNow) && (boardStatus[4] == whoIsNow) && (boardStatus[8] == whoIsNow)) {
+            whereWin = '/';
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    nextRound();
+                }
+            }, winBoardResetDelay);
+            someoneWin = true;
+        } else if ((boardStatus[2] == whoIsNow) && (boardStatus[4] == whoIsNow) && (boardStatus[6] == whoIsNow)) {
+            whereWin = '\\';
             Timer.schedule(new Timer.Task() {
                 @Override
                 public void run() {
@@ -46,6 +105,7 @@ public class GameController {
         }
         whoIsNow = 'x';
         someoneWin = false;
+        whereWin = '#';
     }
 
     private void changePlayer() {
@@ -72,5 +132,9 @@ public class GameController {
 
     public boolean isSomeoneWin() {
         return someoneWin;
+    }
+
+    public char getWhereWin() {
+        return whereWin;
     }
 }
