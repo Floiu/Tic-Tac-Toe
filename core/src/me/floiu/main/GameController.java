df8c0d1a6ec2d25e8ecb06c6223e4c6c1795a11c
DESKTOP-LUKASZ\Lukasz <ratajaczak@gmail.com>
2018-01-20 14:32:08 +0100

    add win check system

--- core/src/me/floiu/main/GameController.java
+++ core/src/me/floiu/main/GameController.java
@@ -1,23 +1,44 @@
 package me.floiu.main;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.utils.Timer;
 
 public class GameController {
 
     private char[] boardStatus = {'#', '#', '#', '#', '#', '#', '#', '#', '#'};
 
     private char whoIsNow = 'x';
+    private float winBoardResetDelay = 0.75f;
 
     public void makeMove(int i, InputEvent event) {
         if (boardStatus[i-1] != '#') {
             event.cancel();
         } else {
             boardStatus[i-1] = whoIsNow;
+            checkWin();
         }
         changePlayer();
     }
 
-    public void resetGame() {
+    private void checkWin() {
+        if (((boardStatus[0] == whoIsNow) && (boardStatus[1] == whoIsNow)&&(boardStatus[2] == whoIsNow)) ||
+                ((boardStatus[3] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[5] == whoIsNow)) ||
+                ((boardStatus[6] == whoIsNow) && (boardStatus[7] == whoIsNow)&&(boardStatus[8] == whoIsNow)) ||
+                ((boardStatus[0] == whoIsNow) && (boardStatus[3] == whoIsNow)&&(boardStatus[6] == whoIsNow)) ||
+                ((boardStatus[1] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[7] == whoIsNow)) ||
+                ((boardStatus[2] == whoIsNow) && (boardStatus[5] == whoIsNow)&&(boardStatus[8] == whoIsNow)) ||
+                ((boardStatus[0] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[8] == whoIsNow)) ||
+                ((boardStatus[2] == whoIsNow) && (boardStatus[4] == whoIsNow)&&(boardStatus[6] == whoIsNow))) {
+            Timer.schedule(new Timer.Task() {
+                @Override
+                public void run() {
+                    clearBoard();
+                }
+            }, winBoardResetDelay);
+        }
+    }
+
+    public void clearBoard() {
         for (int i=1; i<=boardStatus.length; i++) {
             boardStatus[i-1] = '#';
         }
